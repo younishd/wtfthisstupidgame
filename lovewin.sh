21327707c15caf9e1816b12147764cbe9fb984b9
YouniS Bensalah <younis.bensalah@gmail.com>
2016-10-03 22:59:57 +0200

    Script removes .love file as well

--- lovewin.sh
+++ lovewin.sh
@@ -8,5 +8,5 @@ cp -v love-0.10.1-win32/{*.dll,license.txt} wtfthisgame-win32/
 cat love-0.10.1-win32/love.exe wtfthisgame.love > wtfthisgame-win32/wtfthisgame.exe
 zip -r -9 wtfthisgame-win32.zip wtfthisgame-win32
 mv -v wtfthisgame-win32.zip bin/
-rm -rfv wtfthisgame-win32 love-0.10.1-win32 love-0.10.1-win32.zip
+rm -rfv wtfthisgame-win32 wtfthisgame.love love-0.10.1-win32 love-0.10.1-win32.zip
 echo 'done.'
